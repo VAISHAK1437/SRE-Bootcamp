cea5bf6b0ca17aa4e3591dafb0312fcf26d8ca48
Vaishak T Nair <vaishaktnair@gmail.com>
2024-12-10 13:03:14 +0530

    Milestone-2

---
+++ Milestone-2/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load configurations from environment variables
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api/v1')
+
+    return app
