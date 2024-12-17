1e350e762454e80444cc08fecbd9665c1166497a
Vaishak T Nair <vaishaktnair@gmail.com>
2024-12-17 16:20:02 +0530

    linting

--- Milestone-4/app/__init__.py
+++ Milestone-4/app/__init__.py
@@ -1,19 +1,20 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
 
 # Initialize extensions
 db = SQLAlchemy()
 migrate = Migrate()
+
 
 def create_app():
     """Create and configure the Flask application."""
     app = Flask(__name__)
 
     # Load configurations from environment variables
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Initialize extensions
     db.init_app(app)
@@ -21,6 +22,7 @@ def create_app():
 
     # Register blueprints
     from .routes import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api/v1')
+
+    app.register_blueprint(api_bp, url_prefix="/api/v1")
 
     return app
--- Milestone-4/app/models.py
+++ Milestone-4/app/models.py
@@ -1,8 +1,9 @@
 from . import db
 
+
 class Student(db.Model):
     """Model representing a student."""
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     age = db.Column(db.Integer, nullable=False)
